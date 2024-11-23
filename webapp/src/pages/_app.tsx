afb89ea363d513dafb8851306c64c2c1c94a1e8a
Senny Kalidien <sennykalidien@gmail.com>
2024-11-23 10:09:23 +0100

    favicon + manifest

--- webapp/src/pages/_app.tsx
+++ webapp/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { Oxanium, Open_Sans } from "next/font/google";
 import { Layout } from "@/components";
+import Head from "next/head";
 
 const oxanium = Oxanium({
   subsets: ["latin"],
@@ -24,6 +25,23 @@ export const metadata: Metadata = {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${oxanium.variable} ${openSans.variable}`}>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <style jsx global>{`
         body {
           font-family: ${openSans.style.fontFamily};
